6454e328daa360d758c6115a0f6e9c3a6aa2abd2
Aleksandr Kashin <kashin108@gmail.com>
2023-03-08 22:01:51 +0300

    Done task 8

---
+++ task8/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число: ");
+int n = int.Parse(Console.ReadLine()!);
+
+ for (int i = 1; i <= n; i ++)
+     if (i % 2 == 0)
+     Console.Write($"{i} ");
