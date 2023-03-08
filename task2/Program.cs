bc44a322e218a890fd52655829125f09a4780d6b
Aleksandr Kashin <kashin108@gmail.com>
2023-03-08 19:45:34 +0300

    Done task2

---
+++ task2/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+int a = int.Parse(Console.ReadLine()!);
+int b = int.Parse(Console.ReadLine()!);
+
+if (a > b)
+{
+    Console.WriteLine($"{a} - max, {b} - min");
+}
+else if (a < b)
+{
+    Console.WriteLine($"{b} - max, {a} - min");
+}    
+else if (a == b)
+{
+    Console.WriteLine($"{a} и {b} равны");
+}
+    
