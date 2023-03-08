f064a944f523b0a96aa84c1aa30f5be2b0c3f2a8
Aleksandr Kashin <kashin108@gmail.com>
2023-03-08 20:08:03 +0300

    task4 done

---
+++ task4/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+Console.WriteLine("Введите числа:");
+int a = int.Parse(Console.ReadLine()!);
+int b = int.Parse(Console.ReadLine()!);
+int c = int.Parse(Console.ReadLine()!);
+int max;
+
+max = a;
+if (b > a)
+    max = b;
+if (c > max)
+    max = c;
+
+Console.WriteLine($"Максимальное число: {max}");
