db21d5367967392d57c77c29ff024d9bd0cbc22f
Aleksandr Kashin <kashin108@gmail.com>
2023-03-08 20:13:20 +0300

    Done task 6

---
+++ task6/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число:");
+int a = int.Parse(Console.ReadLine()!);
+
+if (a % 2 == 0)
+    Console.WriteLine($"{a} является четным числом");
+else
+    Console.WriteLine($"{a} является нечетным числом");
