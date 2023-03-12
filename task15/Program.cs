5867b9cc7c0859cd94a04499fe6bb440cabfe922
Юлия Кашина <kashinayu@MacBook-Air-Ulia.local>
2023-03-12 13:42:52 +0300

    Done task 15

---
+++ task15/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Clear();
+Console.WriteLine("Введите цифру: ");
+int num = Int32.Parse(Console.ReadLine()!);
+
+if (num == 6 || num == 7)
+    Console.WriteLine("Да");
+else
+    Console.WriteLine("Нет");
