5a94b77b9c08be923fa7ef69f09b0e9d71cfbc3b
Юлия Кашина <kashinayu@MacBook-Air-Ulia.local>
2023-03-12 12:55:59 +0300

    Done task 10 - добавил правки

--- task10/Program.cs
+++ task10/Program.cs
@@ -1,6 +1,6 @@
 ﻿Console.Clear();
 Console.WriteLine("Введите число: ");
 int num = Int32.Parse(Console.ReadLine());
-Console.WriteLine($"Вторым число является: {(num / 10) % 10}");
+Console.WriteLine($"Вторым числом является: {(num / 10) % 10}");
 
 
