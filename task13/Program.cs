49a382fd6ff61e220f8d8d1d4497d16465b975cf
Юлия Кашина <kashinayu@MacBook-Air-Ulia.local>
2023-03-12 13:30:52 +0300

    Done task13

---
+++ task13/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число: ");
+int num = Int32.Parse(Console.ReadLine()!);
+
+if (num > 99 && num < 999)
+    Console.WriteLine($"Третья цифра: {num % 10}");
+else if (num > 999 && num < 9999)
+    Console.WriteLine($"Третья цифра: {num / 10 % 10}");
+else if (num > 9999)
+    Console.WriteLine($"Треть цифра: {num / 100 % 10}");
+else
+    Console.WriteLine("Третьей цифры нет");
