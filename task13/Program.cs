ce899e29660ea88601971263c54d67edb60b512b
Юлия Кашина <kashinayu@MacBook-Air-Ulia.local>
2023-03-14 16:06:31 +0300

    Скорректирована логика решения

--- task13/Program.cs
+++ task13/Program.cs
@@ -1,12 +1,14 @@
 ﻿Console.Clear();
 Console.WriteLine("Введите число: ");
 int num = Int32.Parse(Console.ReadLine()!);
-
-if (num > 99 && num < 999)
-    Console.WriteLine($"Третья цифра: {num % 10}");
-else if (num > 999 && num < 9999)
-    Console.WriteLine($"Третья цифра: {num / 10 % 10}");
-else if (num > 9999)
-    Console.WriteLine($"Треть цифра: {num / 100 % 10}");
-else
-    Console.WriteLine("Третьей цифры нет");
+int result = -1; 
+if (num >= 100){
+    while (num > 999){
+        num = num / 10;
+}
+result = num % 10;
+Console.WriteLine($"Третье число: {result}");
+}   
+else {
+Console.WriteLine($"Третьей цифры нет");
+}
