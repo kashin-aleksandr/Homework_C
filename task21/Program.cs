07f36d8bef2af8487ba49ae177fb44c0e2ef05d8
Aleksandr Kashin <kashin108@gmail.com>
2023-03-17 16:37:33 +0300

    Done task 21

---
+++ task21/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+Console.Write("Введите координату X(1): ");
+double x1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату Y(1): ");
+double y1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату Z(1): ");
+double z1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату X(2): ");
+double x2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату Y(2): ");
+double y2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату Z(2): ");
+double z2 = Convert.ToDouble(Console.ReadLine());
+double s = Math.Sqrt(Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2) + Math.Pow(z1 - z2, 2));
+Console.WriteLine($"Расстояние между тремя точками равно {Math.Round(s, 2)}");
