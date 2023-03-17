9cdc6b2ac19fad0665424ad875541cf164273b6d
Aleksandr Kashin <kashin108@gmail.com>
2023-03-17 16:29:26 +0300

    Решение задачи через массив и метод reverse

--- task19/Program.cs
+++ task19/Program.cs
@@ -1,17 +1,12 @@
-﻿// Данный алгоритм корректно проверяет только шестизначные цифры
+﻿//Решение задачи через массив и метод reverse
 Console.Clear();
-Console.WriteLine("Введите цифру: ");
-int num = Int32.Parse(Console.ReadLine()!);
-int result = num / 100;
-int temp = 0;
+Console.WriteLine("Введите число: ");
+string? num = Console.ReadLine()!;
+char [] array_num = num.ToCharArray();
+Array.Reverse(array_num);
+string final_num = new string(array_num);
 
-for (int i = 0; i <= result; i++){
-    temp += num % 10;
-    if (temp == result)
-        break;
-}
-
-if (temp == result)
+if (num == final_num)
     Console.WriteLine($"{num} является палиндромом");
 else
     Console.WriteLine($"{num} не является палиндромом");
