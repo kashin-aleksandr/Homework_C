adb50d70958a01b5098778a06e2a1043aca1b216
Aleksandr Kashin <kashin108@gmail.com>
2023-03-17 15:53:50 +0300

    Done task 19

---
+++ task19/Program.cs
@@ -0,0 +1,17 @@
+﻿// Данный алгоритм корректно проверяет только шестизначные цифры
+Console.Clear();
+Console.WriteLine("Введите цифру: ");
+int num = Int32.Parse(Console.ReadLine()!);
+int result = num / 100;
+int temp = 0;
+
+for (int i = 0; i <= result; i++){
+    temp += num % 10;
+    if (temp == result)
+        break;
+}
+
+if (temp == result)
+    Console.WriteLine($"{num} является палиндромом");
+else
+    Console.WriteLine($"{num} не является палиндромом");
