393099307bae6c6ffeb55579a733afab5d0e7626
Aleksandr Kashin <kashin108@gmail.com>
2023-03-25 12:22:47 +0300

    Done task 29

---
+++ task29/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Clear();
+Console.Write("Введите количество элементов в массиве: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+for (int i = 0; i < array.Length; i++)
+    array[i] = new Random().Next(1, 17893); 
+Console.WriteLine($"[{string.Join(", ", array)}]");
