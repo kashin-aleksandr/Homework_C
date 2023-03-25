c03a7908fbc255edf346881713fb75c82dd47a31
Aleksandr Kashin <kashin108@gmail.com>
2023-03-25 13:32:35 +0300

    Done task 34

---
+++ task34/Program.cs
@@ -0,0 +1,21 @@
+﻿void Array_nums(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(100,1000);
+    }
+}
+
+Console.Write("Укажите размер массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[size];
+Array_nums(array);
+Console.WriteLine($"Созданный массив: [{string.Join(", ", array)}]");
+
+int count = 0;
+for (int i = 0; i < array.Length; i++)
+if (array[i] % 2 == 0)
+count++;
+
+Console.WriteLine($"В массиве {count} четных числа");
+
