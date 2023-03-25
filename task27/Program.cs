cef3d761bb6456aadb36456a6dce7e2ae262db81
Aleksandr Kashin <kashin108@gmail.com>
2023-03-25 11:56:04 +0300

    Done task 27

---
+++ task27/Program.cs
@@ -0,0 +1,13 @@
+﻿int sum_numbers(int num){
+    int result = 0;
+    for (int i = 0; i <= num; i++){
+        result += num % 10; 
+        num = num / 10; 
+}
+    return result;
+    }
+
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int res = sum_numbers(num);
+Console.WriteLine($"Cумма цифр в числе {num} = " + res);
