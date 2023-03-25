faa67f7289298bd230454258c8bfb91d20800640
Aleksandr Kashin <kashin108@gmail.com>
2023-03-25 11:12:08 +0300

    Done task 25

---
+++ task25/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.Write("Введите число A: ");
+int num_a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число B: ");
+int num_b = Convert.ToInt32(Console.ReadLine());
+int num_c = 1;
+
+while (num_b != 0){
+    num_c = num_c * num_a;
+    num_b = num_b - 1;
+}
+
+Console.Write($"Число A в натуральной степени B равно: {num_c}");
