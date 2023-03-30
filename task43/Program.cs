60bbf25f11cfc377840dd1eb0741ffaec8aacc4c
Kashin Aleksandr <a.v.kashin@mail.msk>
2023-03-30 18:40:30 +0300

    Done task43

---
+++ task43/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+Console.WriteLine("Укажите значение b1");
+double b1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Укажите значение k1");
+double k1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Укажите значение b2");
+double b2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Укажите значение k2");
+double k2 = Convert.ToInt32(Console.ReadLine());
+
+double x = (-b2 + b1)/(-k1 + k2);
+double y = k2 * x + b2;
+
+Console.WriteLine($"Точки пересечения двух прямых: {x}; {y}");
