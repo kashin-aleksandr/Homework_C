cc2d3738723b26279cb7faca02b2a5e0a7d7c92c
Kashin Aleksandr <a.v.kashin@mail.msk>
2023-03-30 18:28:25 +0300

    Done task 41

---
+++ task41/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите числа через пробел: ");
+int[] array_numbers = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+int counter = 0;
+ 
+for (int i = 0; i < array_numbers.Length; i++)
+{
+    if (array_numbers[i] > 0)
+        counter++;
+}
+Console.WriteLine($"Чисел больше нуля: {counter}");
