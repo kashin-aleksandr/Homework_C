c41e8ae4c59d834cc739ef4b179a4fcfb3cc4725
Kashin Aleksandr <a.v.kashin@mail.msk>
2023-04-03 14:45:05 +0300

    Done task64

---
+++ task64/Program.cs
@@ -0,0 +1,14 @@
+﻿int ListNumbers (int UserNumber, int LastNumber)
+{
+    if (UserNumber == LastNumber)
+        return UserNumber;
+    else
+        Console.Write($"{ListNumbers(UserNumber, LastNumber + 1)}, ");
+    return LastNumber;
+}
+
+int LastNumber = 1;
+Console.Write("Введите натуральное число: ");
+int UserNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(ListNumbers(UserNumber, LastNumber));
+
